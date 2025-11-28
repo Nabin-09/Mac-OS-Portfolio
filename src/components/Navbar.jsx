2330239a4e231e298674ced806beae4dd3499695
Nabin <sharmanabin.0910@gmail.com>
2025-11-28 22:39:50 +0530

    Completion of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
-import { navLinks } from '#constants'
+import { navIcons, navLinks } from '#constants'
+import dayjs from 'dayjs'
 import React from 'react'
 
-
-
 const Navbar = () => {
-  return(
+  return (
     <nav>
-        <div>
-            <img src="/images/logo.svg" alt="Logo" />
-            <p className='font-bold'>Nabin's Portfolio</p>
+      {/* LEFT SIDE */}
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className="font-georama font-bold">Nabin's Portfolio</p>
 
-            <ul>
-                {navLinks.map(({id , name})=>(
-                    <li key={id}>
-                        <p>{name}</p>
-                    </li>
-                ))}
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p className="font-georama">{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-            </ul>
-        </div>
+      {/* RIGHT SIDE */}
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time className='font-georama'>{dayjs().format("ddd MM D h:mm A")}</time>
+      </div>
     </nav>
   )
 }
