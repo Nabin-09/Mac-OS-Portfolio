7645da7fbbaa6ee774c4367cd9e07251a2e94e49
Nabin <sharmanabin.0910@gmail.com>
2025-11-29 14:27:20 +0530

    Implementation of home-section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,12 @@
+import { Weight } from "lucide-react";
 import { useRef } from "react"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
-
+const FONT_WEIGHTS = {
+  subtitle : {min : 100 , max : 400 , default : 100},
+  title : {min : 400 , max : 900 , default : 400}
+}
 const renderText = (text, className, baseWeight = 400) => {
   return [...text].map((char, i) => (
     <span
@@ -15,9 +21,54 @@ const renderText = (text, className, baseWeight = 400) => {
   ));
 };
 
+const setupTextHover = (container, type) => {
+  if (!container) return;
+
+  const letters = container.querySelectorAll("span");
+  const { min, max, default: base } = FONT_WEIGHTS[type];
+
+  const animateLetter = (letter, weight, duration = 0.25) => {
+    return gsap.to(letter, {
+      duration,
+      ease: "power2.out",
+      fontVariationSettings: `"wght" ${weight}`
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { left } = container.getBoundingClientRect();
+    const mouseX = e.clientX - left;
+
+    letters.forEach((letter) => {
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX - (l - left + w / 2));
+      const intensity = Math.exp(-(distance ** 2) / 2000);
+
+      animateLetter(letter, min + (max - min) * intensity);
+    });
+  };
+const handleMouseLeave = () => {
+  letters.forEach((letter) => {
+    animateLetter(letter, base, 0.3);
+  });
+};
+
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener('mouseleave' , handleMouseLeave);
+};
+
 const Welcome = () => {
   const titleRef = useRef(null);
   const subtitleRef = useRef(null);
+  useGSAP(()=>{
+    const titleCleanup = setupTextHover(titleRef.current , 'title');
+    const subtileCleanup =  setupTextHover(subtitleRef.current, 'subtitle')
+
+    return ()=>{
+      subtileCleanup();
+      titleCleanup();
+    };
+  } ,[])
 
   return <section id="welcome">
     <p ref={subtitleRef}>
